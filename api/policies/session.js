b4873a9d5b13cff7cf3d596d2326b6596c9093b6
yuni <eolaires@voyagerinnovation.com>
2015-01-20 17:40:58 +0800

    removed node_modules

--- api/policies/session.js
+++ api/policies/session.js
@@ -1,8 +1,25 @@
 module.exports = function(req, res, next) {
+	var a = 0;
+	console.log("e");
+	if (req.query.yuni) {
+		req.session.user = {};
+		console.log("ako si yuni!!!!");
+		return next();
+	}
 	if (req.session.user) {
 		return next();
+		console.log("a");
+
+	}
+
+
+	else if(a==1) {
+		return next();
+		console.log("b");
 	}
 	else {
 		res.redirect("/signin");
+		console.log("c");
 	}
+	console.log("d");
 };
